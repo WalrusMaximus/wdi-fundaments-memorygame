17e1d8e0f5a5f2a043c0297c6f98a3df86dacb43
Matthew freeland <Matthewjfreeland@gmail.com>
2018-12-31 03:04:36 -0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
 var cards = ["queen","queen","king","king"]
 var cardsInPlay = []
-var cardOne = cards[3]
-var cardTwo = cards[2]
-cardsInPlay.push(cardOne)
-cardsInPlay.push(cardTwo)
 
-console.log("User flipped " + cardsInPlay[0]);
-console.log("User flipped " + cardsInPlay[1]);
-
-if (cardsInPlay.length == 2) {
+var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) alert("You found a match!");
 	else alert("Sorry, try again.");
 }
+
+var flipCard = function(cardId) {
+	if (cardsInPlay.length == 2) 
+
+	checkForMatch();
+
+	console.log("User flipped " + cards[cardId])
+	cardsInPlay.push(cards[cardId])
+}
+flipCard(0);
+flipCard(2);
